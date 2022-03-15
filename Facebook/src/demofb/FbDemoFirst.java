0afc253b8eca16ff6ea904f85b1adc0602d5e3e6
pc <pc@Kishor>
2022-03-15 19:24:26 +0530

    facebook commit

---
+++ Facebook/src/demofb/FbDemoFirst.java
@@ -0,0 +1,18 @@
+package demofb;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FbDemoFirst {
+	public static void main(String[] args) {
+		
+        System.setProperty("webdriver.chrome.driver", "F:\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com");
+		
+		
+	}
+
+}
